efdff9178320c0ca3c79028d76fa7737dfec5265
Sair Abbas <sair_abbas@yahoo.com>
2026-07-13 17:52:35 -0700

    switched to Groq API for its free tier

--- main.py
+++ main.py
@@ -1,25 +1,50 @@
 #!/usr/bin/env python3
 
+# from openai import OpenAI
+# from google import genai
 from dotenv import load_dotenv
-from openai import OpenAI
+from groq import Groq
 import os
 
 # Load variables from .env
 load_dotenv()
 
 # Create connection to OpenAI API
-client = OpenAI()
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
+# client = genai.Client(
+#     api_key=os.getenv("GEMINI_API_KEY")
+# )
+# client = OpenAI(
+#     api_key=os.getenv("OPENAI_API_KEY")
+# )
 
 def testAPI():
 
     # Send a request to the model
-    response = client.responses.create(
-        model="gpt-5-mini",
-        input="Explain what an API is in one sentence."
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "user",
+                "content": "Explain what an API is"
+            }
+        ]
     )
+    # response = client.models.generate_content(
+    #     model="gemini-2.0-flash",
+    #     contents="Explain what an API is in one sentence."
+    # )
+    # response = client.responses.create(
+    #     model="gpt-5-mini",
+    #     input="Explain what an API is in one sentence."
+    # )
 
     # Print the AI response
-    print(response.output_text)
+    print(response.choices[0].message.content)
+    #print(response.text)
+    # print(response.output_text)
 
 def main():
     testAPI()
